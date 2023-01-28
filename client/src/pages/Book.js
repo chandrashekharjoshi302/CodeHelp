582ae8838b56820d47746d2c3604520a548dfa34
chandrashekharjoshi302 <chandrashekharjoshi02@gmail.com>
2023-01-28 18:27:01 +0530

    Book page

---
+++ client/src/pages/Book.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Book(){
+    return (
+        <div style={{marginTop:"100px"}}>
+          <a href ="https://drive.google.com/file/d/1TBJHhIe8cjRc9gHwYkMwsfUTlbcWsz0I/view?usp=share_link" target = "_blank">Download Pdf</a>
+        </div>
+    );
+}
+
+
+export default Book;
