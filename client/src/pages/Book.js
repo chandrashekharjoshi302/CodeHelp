e22eddc5ea7155ba5b2cb148110f2284f71fd846
chandrashekharjoshi302 <chandrashekharjoshi02@gmail.com>
2023-01-28 18:28:28 +0530

    Book page

--- client/src/pages/Book.js
+++ client/src/pages/Book.js
@@ -3,7 +3,11 @@ import React from "react";
 function Book(){
     return (
         <div style={{marginTop:"100px"}}>
-          <a href ="https://drive.google.com/file/d/1TBJHhIe8cjRc9gHwYkMwsfUTlbcWsz0I/view?usp=share_link" target = "_blank">Download Pdf</a>
+          <a href ="plz give your goole drive pdf link here" target = "_blank">Coding Book</a>
+          {/* example */}
+          <a href ="https://drive.google.com/file/d/1TBJHhIe8cjRc9gHwYkMwsfUTlbcWsz0I/view?usp=share_link" target = "_blank">DSA Book</a>
+          <a href ="" target = "_blank">Interview Book</a>
+         
         </div>
     );
 }
